1f7b5f6ce43f70a0416e5948bd8edf7489ec73a2
Dulciny <70864330+Dulciny@users.noreply.github.com>
2020-09-25 06:41:50 -0300

    Add files via upload
    a

--- Rede/src/Server.java
+++ Rede/src/Server.java
@@ -1,62 +1,129 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
-public class Server {
+public class Server extends Thread {
 
-	public static void main(String[] args) {
-		
-		ServerSocket server;
-		Socket cliente;
-		Scanner in;
-		String entrada;
-		Boolean chatOn;
-		
-		try {
-			server = new ServerSocket(9876);
-		} catch (Exception e) {
-			System.out.println("Erro ao criar socket na porta 9876");
-			System.out.println("Erro:"+e.getMessage());
-			return;
-		}
-		
+    private static ArrayList<BufferedWriter>clientes;
+    private static ServerSocket server;
+    private String nome;
+    private Socket socket;
+    private InputStream linhaEntrada;
+    private InputStreamReader leitorLinhaEntrada;
+    private BufferedReader BLeitor;  
+    private BufferedWriter BEscritor;
+    
+    public Server(Socket socket){
+        this.socket = socket;
+        try {
+            linhaEntrada  = socket.getInputStream();
+            leitorLinhaEntrada = new InputStreamReader(linhaEntrada);
+            BLeitor = new BufferedReader(leitorLinhaEntrada);
+        } catch (IOException e) {
+        	//e.printStackTrace();
+        }
+    }
 
-		try {
-		    System.out.println("Aguardando conexão...");
-			cliente = server.accept();
-			chatOn = true;
-			System.out.println("Conectado a:"+cliente.getInetAddress().getHostAddress());
-		} catch (Exception e) {
-			System.out.println("Erro ao criar socket na porta 9876");
-			System.out.println("Erro:"+e.getMessage());
-			chatOn = false;
-			return;
-		}
-		
-		try {
-			do {
-			in = new Scanner(cliente.getInputStream());
-			entrada = in.nextLine();
-			System.out.println("Recebido: " + entrada);
-			} while(chatOn = true);
-		} catch (Exception e) {
-			System.out.println("Erro de comunicação");
-			System.out.println("Erro:"+e.getMessage());
-			return;
-		}
-		
-		try {
-			if(entrada.equals("exit")) {
-			   server.close();
-			   cliente.close();
-			   System.out.println("fechando o servidor...");
-			}						
-		} catch (Exception e) {
-			System.out.println("Erro ao encerrar conexão");
-			System.out.println("Erro:"+e.getMessage());
-			return;
-		}
+    public void run(){
+        try{
+            String mensagem = "";
+            OutputStream linhaSaida=this.socket.getOutputStream();
+            Writer escritor = new OutputStreamWriter(linhaSaida);
+            BEscritor=new BufferedWriter(escritor);
+            clientes.add(BEscritor);
+            nome=mensagem=BLeitor.readLine();
+            while(!mensagem.equals(null)){
+                mensagem=BLeitor.readLine();
+                espalharMsg(BEscritor,mensagem);
+                System.out.println(mensagem);
+                if(mensagem.equals("/platypus")) {
+                	espalharMsg(null,"Desligando o servidor...");
+//                	socket.close();
+//            		linhaEntrada.close();
+//            		leitorLinhaEntrada.close();
+//            		BLeitor.close();
+            		System.exit(0);
+                }
+            }
+        }catch (Exception e) {
+        	//e.printStackTrace();
+        }
+    }
+    
+    public void espalharMsg(BufferedWriter BSaida, String mensagem) throws IOException {
+    	BufferedWriter BEscritor;
+    	if(BSaida==null) {
+    		for(BufferedWriter Bescritor:clientes){
+                BEscritor = (BufferedWriter)Bescritor;
+                Bescritor.write("Server: Desligando o servidor...");
+                Bescritor.flush();
+    		}
+    	}else {       
+       try {
+    	   for(BufferedWriter Bescritor:clientes){
+               BEscritor = (BufferedWriter)Bescritor;
+               if(!(BSaida == BEscritor)&&!nome.equals(null)&&!mensagem.equals(null)){
+               	if(nome.equals("")) {
+       			nome = "Anônimo: ";
+                   Bescritor.write(nome+mensagem+"\r\n");
+                   Bescritor.flush();
+               	}
+               	else if(nome.equals("Anônimo: ")) {
+                Bescritor.write(nome+mensagem+"\r\n");
+                Bescritor.flush();
+                }
+                else {
+                Bescritor.write(nome+": "+mensagem+"\r\n");
+                Bescritor.flush();
+               }
+             }
+           }
+       }catch(Exception e) {
+    	  //e.printStackTrace();
+       }
+      }
+    }
 
-	}
-//b
-}
+    public static void main(String []args) {
+
+        try{
+            JLabel lblMessage=new JLabel("Porta do Servidor:");
+            JTextField txtPorta=new JTextField();
+            Object[]texts={lblMessage,txtPorta};
+            JOptionPane.showMessageDialog(null,texts);
+            if(txtPorta.getText().equals(""))
+            	System.exit(0);
+            server=new ServerSocket(Integer.parseInt(txtPorta.getText()));
+            clientes=new ArrayList<BufferedWriter>();
+            JOptionPane.showMessageDialog(null,"Servidor ativo na porta: "+txtPorta.getText());           
+            while(true){
+                System.out.println("Aguardando conexão...");
+                Socket socket = server.accept();
+                String hostName = socket.getInetAddress().getHostName();
+                String aviso = "Cliente " +hostName+" conectado...";
+                System.out.println(aviso);
+                Thread t = new Server(socket);
+                t.start();
+                //espalharMsg(BEscritor,aviso);
+            }
+
+        }catch (Exception e) {
+        	 JLabel lblErro=new JLabel("Erro: Porta já esta em uso ");
+        	 Object[]erro={lblErro};
+        	 JOptionPane.showMessageDialog(null,erro);
+             System.exit(0);
+        }
+    }
+
+} 
